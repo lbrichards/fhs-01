851a0bf4ad0b75b6976a80b2beaa65b31190397c
Larry Richards <larry@source1.jp>
2018-10-02 16:44:55 +0900

    first commit

---
+++ turtle_draw.py
@@ -0,0 +1,22 @@
+import turtle
+import tkinter
+import time
+
+fred = turtle.Turtle()
+fred.speed(.6)
+fred.shape("turtle")
+fred.shapesize(2)
+fred.color('blue')
+fred.forward(100)
+fred.right(60)
+fred.forward(100)
+fred.right(60)
+fred.forward(100)
+fred.right(60)
+fred.forward(100)
+fred.right(60)
+fred.forward(100)
+fred.right(60)
+fred.forward(100)
+fred.right(60)
+tkinter.mainloop()
