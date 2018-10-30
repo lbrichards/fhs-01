040ddb7a78cf2b7689df4c5fb9ca0733c50acc0b
Larry Richards <larry@source1.jp>
2018-10-30 16:40:52 +0900

    modifications to turtle exercise

--- turtle_draw.py
+++ turtle_draw.py
@@ -1,22 +1,35 @@
 import turtle
 import tkinter
-import time
+from random import (
+    choice,
+    randint,
+    random,
+    paretovariate,
+    gammavariate,
+    shuffle
+    )
 
 fred = turtle.Turtle()
-fred.speed(.6)
-fred.shape("turtle")
+fred.speed(5)
+fred.shape("circle")
 fred.shapesize(2)
 fred.color('blue')
-fred.forward(100)
-fred.right(60)
-fred.forward(100)
-fred.right(60)
-fred.forward(100)
-fred.right(60)
-fred.forward(100)
-fred.right(60)
-fred.forward(100)
-fred.right(60)
-fred.forward(100)
-fred.right(60)
+fred.pensize(3)
+for i in range(60):
+    # fred.pensize(randint(1, 10))
+    # fred.color(
+    #    choice([
+    #        'blue',
+    #        'green',
+    #        'red',
+    #        'orange',
+    #        'black',
+    #        'purple',
+    #        'magenta'
+    #    ])
+    # )
+    fred.forward(100)
+    fred.right(59)
+
+
 tkinter.mainloop()
